c0737f51ab45886d823b0c33bb9383af38734371
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-02-10 19:25:40 -0300

    primeiro commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+export const InputContainer = styled.div`
+widht: 100%;
+height: 75px;
+background-color: #AAAAFF;
+
+display:flex;
+align-items:center;
+justify-content:flex-end;
+ 
+font-size:24 px;
+font-family:'Roboto';
+
+
+input{
+  widht: 100/%;
+  height:75px;
+  background-color: #AAAAFF;
+  border:0;
+  display:flex;
+  flex-direction: column;
+  align-items:flex-end;
+  padding: 0 18px;
+  font-size:24px;
+  font-family:'Roboto';
+  color: #FFFFFF;
+}
+`
+
+ 
